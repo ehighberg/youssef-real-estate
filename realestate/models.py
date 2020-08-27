a8d06cee1af582876b793f76a611a5b30dc5bc3b
Errol Highberg <errol.highberg@gmail.com>
2020-08-27 10:52:42 -0400

    migrate Listing

--- realestate/models.py
+++ realestate/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Listing(models.Model):
     price = models.CharField(max_length=20)
     beds = models.IntegerField()
-    baths = models.DecimalField(decimal_places=1)
+    baths = models.DecimalField(max_digits=2, decimal_places=1)
     sq_ft = models.IntegerField()
     address = models.CharField(max_length=100)
     city_state = models.CharField(max_length=100)
