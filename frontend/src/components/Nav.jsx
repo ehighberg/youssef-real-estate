28649ab1bcb769473529ed891c3e254555505432
Errol Highberg <errol.highberg@gmail.com>
2020-08-27 14:38:41 -0400

    configure webpack to recognize .jsx files

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom'
 export default function Nav() {
     return (
         <nav>
-            <NavLink exact to='/' >About Us</NavLink>
+            <NavLink exact to='/'>About Us</NavLink>
+            <NavLink exact to='/closings'>Recent Closings</NavLink>
+            <NavLink exact to='/history'>Learn more about us and Weichert Realty</NavLink>
         </nav>
     )
 }
