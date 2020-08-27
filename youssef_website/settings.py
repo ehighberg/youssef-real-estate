4e94cdbcfe56039a8e596ab47e5e4cb503cb7412
Errol Highberg <errol.highberg@gmail.com>
2020-08-27 10:38:48 -0400

    add realestate

--- youssef_website/settings.py
+++ youssef_website/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product_display.apps.Product_displayConfig'
+    'realestate.apps.RealestateConfig'
 ]
 
 MIDDLEWARE = [
