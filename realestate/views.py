0229b3249e1c6545cbf82d1cbbccfe2c5023ef03
Errol Highberg <errol.highberg@gmail.com>
2020-08-27 11:13:00 -0400

    setup Listing creation

--- realestate/views.py
+++ realestate/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .models import Listing
+from .serializers import ListingSerializer
+from rest_framework import generics
 
-# Create your views here.
+class ListingListCreate(generics.ListCreateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
