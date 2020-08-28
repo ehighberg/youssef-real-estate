fbcbdfbb9fc209c9416f3a0a9bf5282e6edb215c
Errol Highberg <errol.highberg@gmail.com>
2020-08-28 11:24:13 -0400

    restore to just JSONRenderer renderer classes

--- youssef_website/settings.py
+++ youssef_website/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
