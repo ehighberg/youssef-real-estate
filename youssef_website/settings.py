75610a0afc6431317865fd56f970333850fe953a
Errol Highberg <errol.highberg@gmail.com>
2020-10-05 14:33:17 -0400

    move secrets to youssef_website/.env

--- youssef_website/settings.py
+++ youssef_website/settings.py
@@ -11,6 +11,28 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Import configuration settings from .env
+env = environ.Env(DEBUG=(bool, False))
+
+environ.Env.read_env()
+DEBUG = env('DEBUG')
+
+SECRET_KEY = env('SECRET_KEY')
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': env.db('SQLITE_URL')
+}
+
+CACHES = {
+    'default': env.cache(),
+}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -19,11 +41,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ujt!(0l8)uqa5v1yjhz2^)17v*xqxt$9d&y&q1crg7+2forrf3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -71,17 +88,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'youssef_website.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
